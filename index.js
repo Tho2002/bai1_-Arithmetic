bd136bd34f71bce7e3630434f852ca5537b4400b
tho <ldtho1604@gmail.com>
2024-02-20 21:47:48 +0700

    add

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 //Toán tử gán
 
+// var a = 10;
+// var b = 20;
+// b += a; //b=b+a
+// b -= a; //b=b-a
+// b *= a; //b=b*a
+// b %= a; //b=b%a
+// console.log(b);
+// //Toán tử so sanh
+// var c = 20;
+// var d = 20;
+// var e = c == d;
+// console.log(e);
+// //Toan tu logic
 var a = 10;
 var b = 20;
-b += a; //b=b+a
-b -= a; //b=b-a
-b *= a; //b=b*a
-b %= a; //b=b%a
-console.log(b);
-//Toán tử gán
+var c = 30;
+var d = 40;
+var e = a < b && c < d; //trả về true khi 2 toán hạng đều true
+// var e = a < b || c > d; //Trả về true khi 1 trong 2 là true
+// ! là để chuyển đổi giá trị từ true sang false hoặc ngược laị
+console.log(e);
