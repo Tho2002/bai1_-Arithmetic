b9df32984c004b863d073c7297f17f97bafc59c6
tho <ldtho1604@gmail.com>
2024-02-20 22:27:36 +0700

    xog

--- index.js
+++ index.js
@@ -13,11 +13,24 @@
 // var e = c == d;
 // console.log(e);
 // //Toan tu logic
-var a = 10;
-var b = 20;
-var c = 30;
-var d = 40;
-var e = a < b && c < d; //trả về true khi 2 toán hạng đều true
-// var e = a < b || c > d; //Trả về true khi 1 trong 2 là true
-// ! là để chuyển đổi giá trị từ true sang false hoặc ngược laị
-console.log(e);
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// var d = 40;
+// var e = a < b && c < d; //trả về true khi 2 toán hạng đều true
+// // var e = a < b || c > d; //Trả về true khi 1 trong 2 là true
+// // ! là để chuyển đổi giá trị từ true sang false hoặc ngược laị
+// console.log(e);
+// Kiểu dữ liệu
+//dữ liệu phức tạp
+// function tinhTong(a, b, c) {
+//   return a + b + c;
+// }
+
+var r = 20;
+function dienTich() {
+  return r * 3.14;
+}
+var s1 = r * 3.14;
+
+console.log(s1);
